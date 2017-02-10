ade0e709b77a83f0f57f620a73c7a60cb2d951f9
JustinW <juswelch@gmail.com>
2017-02-10 23:17:17 +0000

    added proxy_protocol to listen directive to pass websockets through ALB properly

--- startservices.sh
+++ startservices.sh
@@ -6,8 +6,6 @@ rm /etc/nginx/site.conf.template
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
 rm /etc/nginx/nginx.conf.template
-#/bin/bash -c "envsubst '\$GOOGLE_CLIENT_ID \$GOOGLE_CLIENT_SECRET \$COOKIE_SECRET' < /etc/oauth2_proxy/oauth2.config.template > /etc/oauth2_proxy/oauth2.config"
-#rm /etc/oauth2_proxy/oauth2.config.template
 
 #bring up nginx
 service nginx start
