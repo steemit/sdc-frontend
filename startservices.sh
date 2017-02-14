418398513d855f7529e7dd88a5783581e0643452
JustinW <juswelch@gmail.com>
2017-02-14 22:01:51 +0000

    move https check into location block, separate health path, default env vars

--- startservices.sh
+++ startservices.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ -z "$SERVER_NAME" ]]; then export SERVER_NAME=steemitdev.com; fi
+
+if [[ -z "$WSPA_SERVER" ]]; then export WSPA_SERVER=steemd.steemitdev.com; fi
+
 # generate nginx config on the fly and feed in any appropriate environment variables
 /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER' < /etc/nginx/site.conf.template > /etc/nginx/sites-enabled/default"
 rm /etc/nginx/site.conf.template
