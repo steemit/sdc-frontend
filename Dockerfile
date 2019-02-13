1ffb90cae9b0b667619047371f753da40b5a9064
justinw <juswelch@gmail.com>
2019-02-13 14:21:09 -0500

    add templates for social wallet split

--- Dockerfile
+++ Dockerfile
@@ -12,9 +12,7 @@ ADD startservices.sh /usr/bin/startservices.sh
 
 RUN chmod +x /usr/bin/startservices.sh
 
-ADD site.conf.template /etc/nginx/site.conf.template
-
-ADD site.devstage.conf.template /etc/nginx/site.devstage.conf.template
+ADD site* /etc/nginx
 
 ADD nginx.conf.template /etc/nginx/nginx.conf.template
 
--- startservices.sh
+++ startservices.sh
@@ -5,13 +5,23 @@ if [[ -z "$SERVER_NAME" ]]; then export SERVER_NAME=steemitdev.com; fi
 if [[ -z "$WSPA_SERVER" ]]; then export WSPA_SERVER=steemd.steemitdev.com; fi
 
 # generate nginx config on the fly and feed in any appropriate environment variables
-if [[ "$NO_ROBOTS" ]]; then
-  /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER' < /etc/nginx/site.devstage.conf.template > /etc/nginx/sites-enabled/default"
+if [[ "$IS_SOCIAL_SITE" ]]; then
+  if [[ "$NO_ROBOTS" ]]; then
+    /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER \$WALLET_URL' < /etc/nginx/site.social.devstage.conf.template > /etc/nginx/sites-enabled/default"
+  else
+    /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER \$WALLET_URL' < /etc/nginx/site.social.conf.template > /etc/nginx/sites-enabled/default"
+  fi
 else
-  /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER' < /etc/nginx/site.conf.template > /etc/nginx/sites-enabled/default"
+  if [[ "$NO_ROBOTS" ]]; then
+    /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER' < /etc/nginx/site.devstage.conf.template > /etc/nginx/sites-enabled/default"
+  else
+    /bin/bash -c "envsubst '\$SERVER_NAME \$WSPA_SERVER' < /etc/nginx/site.conf.template > /etc/nginx/sites-enabled/default"
+  fi
 fi
 rm /etc/nginx/site.conf.template
 rm /etc/nginx/site.devstage.conf.template
+rm /etc/nginx/site.social.conf.template
+rm /etc/nginx/site.social.devstage.conf.template
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 cp /etc/nginx/nginx.conf.template /etc/nginx/nginx.conf
 rm /etc/nginx/nginx.conf.template
